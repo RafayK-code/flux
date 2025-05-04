b832e4dc86fbf704990e09a5a89045aa5e1bf5a7
Rafay Kashif <kashifrafay075@gmail.com>
2025-05-04 00:35:30 -0400

    add shell scripts, math header, and add utility to pch

---
+++ src/flux/util/Math.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <flux/core/base.h>
+
+namespace flux
+{
+    class FLUX_API Math
+    {
+    public:
+
+    };
+}
--- src/pch.h
+++ src/pch.h
@@ -7,3 +7,4 @@
 #include <unordered_set>
 #include <string>
 #include <memory>
+#include <utility>

db6fe36182090972d73c37c3b994f99c04d264cf
Rafay Kashif <rafayka@bigbluebubble.com>
2025-06-19 11:01:35 -0400

    add flux header. Similar usage to SDL

---
+++ runtime/include/flux.h
@@ -0,0 +1,33 @@
+#include <fxpch.h>
+
+#include <flux/core/base.h>
+#include <flux/core/Ref.h>
+
+#include <flux/core/Logger.h>
+#include <flux/core/LogManager.h>
+
+#include <flux/core/delegate/Delegate.h>
+#include <flux/core/delegate/MutliCastDelegate.h>
+
+#include <flux/renderer/Framebuffer.h>
+#include <flux/renderer/GraphicsContext.h>
+#include <flux/renderer/Image.h>
+#include <flux/renderer/IndexBuffer.h>
+#include <flux/renderer/OrthographicCamera.h>
+#include <flux/renderer/PerspectiveCamera.h>
+#include <flux/renderer/Pipeline.h>
+#include <flux/renderer/RenderCommandBuffer.h>
+#include <flux/renderer/Renderer2D.h>
+#include <flux/renderer/RendererAPI.h>
+#include <flux/renderer/RenderPass.h>
+#include <flux/renderer/RenderStates.h>
+#include <flux/renderer/Shader.h>
+#include <flux/renderer/ShaderInputSet.h>
+#include <flux/renderer/UniformBuffer.h>
+#include <flux/renderer/VertexArray.h>
+#include <flux/renderer/VertexBuffer.h>
+
+#include <flux/util/Math.h>
+#include <flux/util/Singleton.h>
+
+#include <flux/window/Window.h>
